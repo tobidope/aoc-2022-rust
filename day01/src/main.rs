09302019ba57dbb06f0098e3290a302c171bef21
Tobias Bell <tobias.bell@gmail.com>
2022-12-01 15:18:19 +0100

    First day

---
+++ day01/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::BinaryHeap;
+
+const INPUT: &str = include_str!("../input.txt");
+fn main() {
+    println!("{}", max_calories(INPUT));
+    println!("{}", top_three_calories(INPUT));
+}
+
+fn max_calories(input: &str) -> usize {
+    let mut max: usize = 0;
+    let mut current: usize = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            max = if current > max { current } else { max };
+            current = 0;
+        } else {
+            let weight: usize = line.parse().unwrap();
+            current += weight;
+        }
+    }
+    max
+}
+
+fn top_three_calories(input: &str) -> usize {
+    let mut calories = Vec::new();
+
+    let mut current = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            calories.push(current);
+            current = 0;
+        } else {
+            let weight: usize = line.parse().unwrap();
+            current += weight;
+        }
+    }
+    calories.sort();
+    let len = calories.len();
+    calories[len - 3] + calories[len - 2] + calories[len - 1]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn elf_with_highest_calories() {
+        assert_eq!(74711, max_calories(INPUT));
+    }
+
+    #[test]
+    fn sum_of_three_highest_calories() {
+        assert_eq!(209481, top_three_calories(INPUT));
+    }
+}
