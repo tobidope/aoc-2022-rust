8f7c951850506731f7f9563a0defe78c3e8d8fb4
Tobias Bell <tobias.bell@gmail.com>
2022-12-04 09:39:34 +0100

    Day 4 part 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,78 @@
+use std::{ops::RangeInclusive, str::FromStr};
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<Assignment> = INPUT.lines().map(|l| l.parse().unwrap()).collect();
+    let result = part1(&input);
+    println!("{result}");
+}
+
+fn part1(input: &[Assignment]) -> usize {
+    input.iter().filter(|a| a.fully_contains()).count()
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Assignment {
+    first: RangeInclusive<usize>,
+    second: RangeInclusive<usize>,
+}
+
+impl Assignment {
+    fn fully_contains(&self) -> bool {
+        (self.first.contains(self.second.start()) && self.first.contains(self.second.end()))
+            || (self.second.contains(self.first.start()) && self.second.contains(self.first.end()))
+    }
+}
+
+impl FromStr for Assignment {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(',');
+        let numbers = parts
+            .flat_map(|p| p.split('-'))
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+        if numbers.len() != 4 {
+            Err(format!("Couldn't parse {}", s))
+        } else {
+            Ok(Self {
+                first: numbers[0]..=numbers[1],
+                second: numbers[2]..=numbers[3],
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_overlapping() {
+        let a = Assignment {
+            first: (2..=8),
+            second: (3..=7),
+        };
+        assert!(a.fully_contains());
+
+        let a = Assignment {
+            first: 2..=4,
+            second: 6..=8,
+        };
+        assert!(!a.fully_contains())
+    }
+
+    #[test]
+    fn test_from_str() {
+        let a: Assignment = "2-8,3-7".parse().unwrap();
+        assert_eq!(
+            Assignment {
+                first: 2..=8,
+                second: 3..=7
+            },
+            a
+        );
+    }
 }
