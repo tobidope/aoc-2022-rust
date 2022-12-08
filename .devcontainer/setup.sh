e90486cd94d25d3252396b576e2bb8f82d5abf16
Tobias Bell <tobias.bell@gmail.com>
2022-12-08 15:52:13 +0100

    Codespace maybe

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,21 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+rustup install stable
+rustup component add rustfmt
+rustup component add clippy 
+
+cargo install cargo-expand
+cargo install cargo-edit
