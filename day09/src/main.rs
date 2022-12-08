b23ed3c3eca27a498ca957c93ab9203859eb9686
Tobias Bell <tobias.bell@gmail.com>
2022-12-08 23:29:02 +0000

    Day 9 can come

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,3 +1,13 @@
+const INPUT: &str = include_str!("../input.txt");
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {}
 }
