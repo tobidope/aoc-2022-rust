3de8061591bd8fc98e8e63dcea5e33d5497834ef
Tobias Bell <tobias.bell@gmail.com>
2022-12-09 11:34:02 +0000

    Not ready

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,14 +1,32 @@
-use std::{collections::HashSet, ops::Add, str::FromStr};
+use std::{
+    collections::HashSet,
+    ops::{Add, Sub},
+    str::FromStr,
+};
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    let head = Position::default();
-    let tail = Position::default();
+    let mut head = Position::default();
+    let mut tail = Position::default();
     let mut visited = HashSet::new();
     visited.insert(tail);
 
     let steps: Vec<Position> = INPUT.lines().map(|l| l.parse().unwrap()).collect();
+    for step in steps {
+        head.step(&step);
+        let diff = head - step;
+        match diff {
+            Position { x: 0, y: 0 } => (),
+            Position { x, y: 0 } if x.abs() > 1 => {
+                tail.x += x;
+            }
+            Position { x: 0, y } if y.abs() > 1 => {
+                tail.y += y;
+            }
+        }
+        visited.insert(tail);
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Default, Clone, Copy)]
@@ -21,6 +39,17 @@ impl Position {
     fn step(&mut self, rhs: &Position) {
         self.x += rhs.x;
         self.y += rhs.y;
+    }
+}
+
+impl Sub for Position {
+    type Output = Position;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
     }
 }
 
