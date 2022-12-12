b710b550103c2c49122d170ee3906e35b9eee81a
Tobias Bell <tobias.bell@gmail.com>
2022-12-12 22:05:58 +0100

    Revert "Refactor"
    This reverts commit 3d44efb06f7b1e483f53b1bb74375ffc6c589fbd.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -29,18 +29,16 @@ fn bfs(map: &[Vec<u8>], start: &Point, end: &Point) -> usize {
     let mut visited = HashSet::new();
     visited.insert((0, 0));
     while let Some((node, distance)) = queue.pop_back() {
-        let height = map[node.0][node.1];
+        let height = map[node.0 as usize][node.1 as usize];
         if node == *end {
             return distance;
         }
 
-        for neighbor in [
-            (node.0 + 1, node.1),
-            (node.0 - 1, node.1),
-            (node.0, node.1 + 1),
-            (node.0, node.1 - 1),
-        ] {
-            if let Some(&neighbor_height) = map.get(neighbor.0).and_then(|row| row.get(neighbor.1))
+        for (i, j) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            let neighbor = (node.0 + i, node.1 + j);
+            if let Some(&neighbor_height) = map
+                .get(neighbor.0 as usize)
+                .and_then(|row| row.get(neighbor.1 as usize))
             {
                 if neighbor_height <= height + 1 && !visited.contains(&neighbor) {
                     visited.insert(neighbor);
@@ -52,7 +50,7 @@ fn bfs(map: &[Vec<u8>], start: &Point, end: &Point) -> usize {
     usize::MAX
 }
 
-type Point = (usize, usize);
+type Point = (i32, i32);
 struct Map {
     start: Point,
     end: Point,
@@ -65,7 +63,7 @@ impl Map {
         for (i, row) in self.map.iter().enumerate() {
             for (j, &value) in row.iter().enumerate() {
                 if value == b'a' {
-                    points.push((i, j));
+                    points.push((i as i32, j as i32));
                 }
             }
         }
@@ -82,10 +80,10 @@ impl FromStr for Map {
         let mut map = vec![];
         for (row, line) in s.lines().enumerate() {
             if let Some(column) = line.find('S') {
-                start = (row, column);
+                start = (row as i32, column as i32);
             }
             if let Some(column) = line.find('E') {
-                end = (row, column);
+                end = (row as i32, column as i32);
             }
             map.push(
                 line.bytes()
